ba37daf7c8c396c8b15a3922975b61266eaf298c
Péter Bozsó <peter.bozso@outlook.com>
2022-01-28 21:19:53 +0100

    Add workaround for scoped services

--- FluxorScopedServiceExample/FluxorScopedServiceExample/Store/CounterUseCase/Effects.cs
+++ FluxorScopedServiceExample/FluxorScopedServiceExample/Store/CounterUseCase/Effects.cs
@@ -5,16 +5,22 @@ namespace FluxorScopedServiceExample.Store.CounterUseCase;
 public class Effects
 {
     private readonly ScopedService _scopedService;
+    private readonly IServiceScopeFactory _scopeFactory;
 
-    public Effects(ScopedService scopedService)
+    public Effects(ScopedService scopedService, IServiceScopeFactory scopeFactory)
     {
         _scopedService = scopedService;
+        _scopeFactory = scopeFactory;
     }
 
     [EffectMethod]
     public Task HandleIncrementCounterAction(IncrementCounterAction action, IDispatcher dispatcher)
     {
-        Console.WriteLine(_scopedService.Id);
+        using var scope = _scopeFactory.CreateAsyncScope();
+        var trulyScopedService = scope.ServiceProvider.GetRequiredService<ScopedService>();
+
+        Console.WriteLine($"Injected service ID: {_scopedService.Id} Truly scoped service ID: {trulyScopedService.Id}");
+
         return Task.CompletedTask;
     }
 }
